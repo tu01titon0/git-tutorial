9128073cdb95d2f1f95e4d50c4c34aa3e8fecd81
tu <nguyenmanhtu09042000@gmail.com>
2020-05-04 17:32:32 +0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_101517) do
+ActiveRecord::Schema.define(version: 2020_05_04_102638) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
   end
 
 end
